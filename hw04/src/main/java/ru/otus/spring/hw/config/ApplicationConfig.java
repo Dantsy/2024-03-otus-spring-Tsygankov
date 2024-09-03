ebe1f9a86063d53ce69c98602cd85562569e4db7
Dantsy <dantsy@yandex.ru>
2024-09-03 14:08:06 +0300

    hw04

--- hw04/src/main/java/ru/otus/spring/hw/config/ApplicationConfig.java
+++ hw04/src/main/java/ru/otus/spring/hw/config/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package ru.otus.spring.hw.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
